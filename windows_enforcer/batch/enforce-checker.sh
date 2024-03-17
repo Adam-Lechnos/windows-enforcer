7c0a6449651dea4206fb3044c011604b5924afe3
Adam Lechnos <email@adamlechnos.com>
2024-03-17 19:05:20 -0400

    updated folder structure

---
+++ windows_enforcer/batch/enforce-checker.sh
@@ -0,0 +1,289 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+outputlist="./enforce-issues.txt"
+
+if [ -f $outputlist ]
+then
+    rm $outputlist
+fi
+
+#echo "[info] Working Directory: $(pwd)" >> $outputlist
+
+## check for install management input files and directories and create if missing
+if ! [ -f ./winget-installs.txt ]
+then
+  touch ./winget-installs.txt
+  echo "install management file missing and re-created - winget-installs.txt" >> $outputlist
+fi
+
+if ! [ -f ./winget-uninstalls.txt ]
+then
+  touch ./winget-uninstalls.txt
+  echo "install management file missing and re-created - winget-uninstalls.txt" >> $outputlist
+fi
+
+if ! [ -f ./github-release-install.txt ]
+then
+  touch ./github-release-install.txt
+  echo "install management file missing and re-created - github-release-install.txt" >> $outputlist
+fi
+
+if ! [ -f ./github-raw-installs.txt ]
+then
+  touch ./github-raw-installs.txt
+  echo "install management file missing and re-created - github-raw-installs.txt" >> $outputlist
+fi
+
+if ! [ -d ./install-removal ]
+then
+  mkdir ./install-removal
+  echo "install management directory missing and re-created - install-removal" >> $outputlist
+fi
+
+if ! [ -d ./cert-removal ]
+then
+  mkdir ./cert-removal
+  echo "cert removal directory missing and re-created - cert-removal" >> $outputlist
+fi
+
+# ensuring scripts folder exists
+if ! [ -d ./../../scripts ]
+then
+  mkdir ./../../scripts
+  echo "scripts folder missing and re-created - script management" >> $outputlist
+fi
+
+# restore backup copy of certificate-present-list if the original file is missing or corrupt
+if ! [ -f ./cert-removal/certificate-present-list.txt ] && [ -f ./cert-removal/certificate-present-list-backup.txt ]
+then
+  mv ./cert-removal/certificate-present-list-backup.txt ./cert-removal/certificate-present-list.txt
+  echo "cert removal list - restored backup copy of 'certificate-present-list'" >> $outputlist
+fi
+
+# create on empty certificate-present-list both original and backup copies are missing. This will cause temporary revokation of existing certs.
+if ! [ -f ./cert-removal/certificate-present-list.txt ]
+then
+  touch ./cert-removal/certificate-present-list.txt
+  echo "cert removal list - no 'certificate-present-list' or backup copy exists. An empty list was created. Existing cert files in 'trusted-root-certifcates' folder will be revoked then re-added on the next two client runs respectively." >> $outputlist
+fi
+
+## ensure proper carriage returns in input files
+sed -i -e 's/\r$//' ./winget-installs.txt 2>&1>/dev/null
+sed -i -e 's/\r$//' ./winget-uninstalls.txt 2>&1>/dev/null
+sed -i -e 's/\r$//' ./github-release-install.txt 2>&1>/dev/null
+sed -i -e 's/\r$//' ./github-raw-installs.txt 2>&1>/dev/null
+sed -i -e 's/\r$//' ./cert-removal/cert-revoked.txt 2>&1>/dev/null
+sed -i -e 's/\r$//' ./cert-removal/certificate-present-list.txt 2>&1>/dev/null
+
+## check winget list
+listwinget="./winget-installs.txt"
+while read -r line || [ -n "$line" ]
+do
+  curl --retry 5 -s https://winstall.app/apps/$line | grep 'To install' >/dev/null
+  rc=$?
+  if (($rc == 1))
+  then
+    echo winget issue = $line >> $outputlist
+  fi
+done < "$listwinget"
+
+## check winget uninstall list
+listwingetrm="./winget-uninstalls.txt"
+while read -r line || [ -n "$line" ]
+do
+  curl -s https://winstall.app/apps/$line | grep 'To install' >/dev/null
+  rc=$?
+  if (($rc == 1))
+  then
+    echo winget uninstall issue = $line >> $outputlist
+  fi
+
+  cat ./winget-installs.txt | grep -w "$line" >/dev/null
+  rc2=$?
+  if (($rc2 == 0))
+  then
+    echo winget uninstall conflict with install input = $line >> $outputlist
+  fi
+done < "$listwingetrm"
+
+## check github release list
+listghrelease="./github-release-install.txt"
+while read -r line || [ -n "$line" ]
+do
+  curl -ks https://api.github.com/repos/$line/releases/latest ^| grep "browser_download_url" >/dev/null
+  rc=$?
+  #echo $rc
+  if ((rc == 1))
+  then
+    echo gihtub release issue = $line >> $outputlist
+  fi
+done < "$listghrelease"
+
+## check github raw list
+listghrelease="./github-raw-installs.txt"
+while read -r line || [ -n "$line" ]
+do
+  sc=$(curl -s -o /dev/null -w '%{http_code}' $line)
+  #echo $sc
+  if [ $sc -ne 200 ]
+  then
+    echo gihtub raw issue = $line >> $outputlist
+  fi
+done < "$listghrelease"
+
+## feature flag folder and file checker
+fffolder="./featureFlags-first-run_enforcement_checks"
+if [ ! -d $fffolder ]
+then 
+  mkdir featureFlags-first-run_enforcement_checks
+  echo "feature flag = [$fffolder] parent folder missing and was re-created" >> $outputlist
+fi
+
+## cert force refresh list checker
+if [ ! -f $fffolder/certificate-refresh_FORCE_renameMe-*.txt ]
+then
+  touch $fffolder/certificate-refresh_FORCE_renameMe-OFF.txt
+  echo "feature flag = [certificate-refresh_FORCE_renameMe-OFF.txt] file missing and was re-created" >> $outputlist
+fi
+## ensure proper carriage returns
+sed -i -e 's/\r$//' $fffolder/certificate-refresh_FORCE_renameMe-*.txt
+
+## check certificate force refresh list
+certforceref=$fffolder/certificate-refresh_FORCE_renameMe-ON.txt
+if [ -f $certforceref ]
+then
+  while read -r line || [ -n "$line" ]
+  do
+    if nslookup $line | grep -w 'No answer'
+    then
+      echo "cert force refresh list - '$line' is not an accessible hostname. Correct the hostname within the refresh list" >> $outputlist
+    fi
+  done < "$certforceref"
+fi
+
+## trusted root certificate checks
+rootcertdir="../../trusted-root-certificates"
+#echo "[info] Root Cert Directory: $rootcertdir" >> $outputlist
+if [ ! -d $rootcertdir ]
+then
+  mkdir $rootcertdir
+fi
+
+# if [ -f $fffolder/certificate-refresh_renameMe-ON.txt ]
+# then
+#   rm $fffolder/certificate-refresh_renameMe-ON.txt
+#   echo $fffolder/certificate-refresh_renameMe-ON.txt
+# fi
+
+if [ -f $fffolder/certificate-refresh_FORCE_LIST.txt ] || [ -f $fffolder/certificate-refresh_FORCE_renameMe-OFF.txt ]
+then
+  rm -f $fffolder/certificate-refresh_FORCE_LIST.txt 
+fi
+
+if [ -z "$(ls -A $rootcertdir)" ]
+then
+
+  echo "cert folder $rootcertdir is empty - skipping enumeration of certifcates"
+  listlastruncrt="./cert-removal/certificate-present-list.txt"
+  while read -r line || [ -n "$line" ]
+  do
+    echo $line >> ./cert-removal/cert-revoked.txt
+    sed -i "s/$line//" ./cert-removal/certificate-present-list.txt 2>&1>/dev/null
+    echo cert revocation - $line >> $outputlist
+  done < "$listlastruncrt"
+  awk -i inplace '!seen[$0]++' ./cert-removal/cert-revoked.txt 2>&1>/dev/null
+  sed -i '/^$/d' ./cert-removal/certificate-present-list.txt 2>&1>/dev/null
+  sed -i '/^$/d' ./cert-removal/cert-revoked.txt 2>&1>/dev/null
+
+else
+
+  for certfile in $rootcertdir/*
+  do
+    
+    ## create list of certs to check for removal on current run
+    listpresentcertscr=$(openssl x509 -in $certfile -noout -subject | sed -e "s/ //g" | sed -n '/^subject/s/^.*CN=//p')
+    echo $listpresentcertscr >> ./cert-removal/certificate-present-list-current-run.txt
+
+    ## reconcile both cert removal lists to check for cert removal action
+    listlastruncrt="./cert-removal/certificate-present-list.txt"
+    while read -r line || [ -n "$line" ]
+    do
+      if ! cat ./cert-removal/certificate-present-list-current-run.txt | grep -w $line >/dev/null
+      then
+        echo $line >> ./cert-removal/cert-revoked.txt
+        sed -i "s/$line//" ./cert-removal/certificate-present-list.txt 2>&1>/dev/null
+        echo cert revocation - $line >> $outputlist
+      fi
+    done < "$listlastruncrt"
+    awk -i inplace '!seen[$0]++' ./cert-removal/cert-revoked.txt 2>&1>/dev/null
+    sed -i '/^$/d' ./cert-removal/cert-revoked.txt
+
+    ## create list of certs to check for removal on next run
+    listpresentcerts=$(openssl x509 -in $certfile -noout -subject | sed -e "s/ //g" | sed -n '/^subject/s/^.*CN=//p')
+    if ! cat ./cert-removal/certificate-present-list.txt | grep -w $listpresentcerts >/dev/null
+    then
+      echo $listpresentcerts >> ./cert-removal/certificate-present-list.txt
+    fi
+    sed -i '/^$/d' ./cert-removal/certificate-present-list.txt 2>&1>/dev/null
+    rm -f ./cert-removal/certificate-present-list-current-run.txt 2>&1>/dev/null
+
+    ## remove cert from revocation list if cert CN is the same as a revoked cert - for newly added certs (assumes new expiration date and fingerprint)
+    removerevoke=$(openssl x509 -in $certfile -noout -subject | sed -e "s/ //g" | sed -n '/^subject/s/^.*CN=//p')
+    if cat ./cert-removal/cert-revoked.txt | grep -w $removerevoke >/dev/null
+    then
+      sed -i "s/$removerevoke//" ./cert-removal/cert-revoked.txt 2>&1>/dev/null
+      sed -i '/^$/d' ./cert-removal/cert-revoked.txt
+      echo cert revocation removal - $removerevoke >> $outputlist
+    fi
+
+    ## force refresh cert list for opted in hosts
+    if [ -f $fffolder/certificate-refresh_FORCE_renameMe-ON.txt ]
+    then
+      listcertforce=$(openssl x509 -in $certfile -noout -subject | sed -e "s/ //g" | sed -n '/^subject/s/^.*CN=//p')
+      echo $listcertforce >> $fffolder/certificate-refresh_FORCE_LIST.txt
+      echo "cert force refresh active = [certificate-refresh_FORCE_renameMe-ON.txt] is active - opted in hosts will first remove all available certs within '$rootcertdir' for hosts listed in 'certificate-refresh_FORCE_LIST.txt'" >> $outputlist
+    fi
+
+    ## warning at 45 days or less expiry
+    if ! openssl x509 -in $certfile -checkend 3888000 -noout >/dev/null
+    then
+      echo "cert issue = [$certfile] warning for cert expiry within 45 days or less of the cert's expiry; renewal must occur at least 15 days prior to the expiry and replaced within the 'trusted-root-certificates' directory" >> $outputlist
+    fi
+
+    ## add the cert to feature flag file for replacement at 14 days or less expiry
+    if ! openssl x509 -in $certfile -checkend 1209600 -noout >/dev/null
+    then
+    
+      echo "cert issue = [$certfile] cert expiry has crossed the threshold warning set at 2 weeks before or less then the expiration date of the cert or cert is invalid" >> $outputlist
+      certname=$(openssl x509 -in $certfile -noout -subject | sed -e "s/ //g" | sed -n '/^subject/s/^.*CN=//p')
+      if ! grep -w $certname $fffolder/certificate-refresh_renameMe-ON.txt
+      then
+        echo $certname >> $fffolder/certificate-refresh_renameMe-ON.txt
+        echo "cert issue = [$certfile] added to feature flag file for cert refresh with CN=$certname" >> $outputlist
+      fi
+
+    else
+      certnameRem=$(openssl x509 -in $certfile -noout -subject | sed -e "s/ //g" | sed -n '/^subject/s/^.*CN=//p')
+      sed -i $fffolder/certificate-refresh_renameMe-ON.txt -e "s/$certnameRem//g"
+    fi
+
+   ## check if cert was removed from the trusted-root-certificates directory
+
+
+  done
+  ## remove empty lines from cert feature flag file
+  sed -i '/^$/d' $fffolder/certificate-refresh_renameMe-ON.txt
+
+fi
+
+## make backup copies of cert removal management lists
+cp ./cert-removal/cert-revoked.txt ./cert-removal/cert-revoked-backup.txt
+cp ./cert-removal/certificate-present-list.txt ./cert-removal/certificate-present-list-backup.txt
+
+## if enforce warnings or issues exist, send as email attachment
+if [ -f  $outputlist ]
+then
+    echo "emailing list"
+    echo "Enforce checker script has detected info, issues, and warnings. Refer to the attached text file for details." | mail -A "./enforce-issues.txt" -s "Host Enforcement Checker" -- adam.lechnos@gmail.com
+fi
---
+++ windows_enforcer/batch/first_run_enforcement_checks.bat
@@ -0,0 +1,301 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set TIMESTAMP=%DATE:/=-%_%TIME::=-%
+set TIMESTAMP=%TIMESTAMP: =%
+set LOGFILE=%temp%\damo_net\logs\first-run-enforcement-%TIMESTAMP%.log
+
+:: Client side folder/file structure enforcement to prevent conflict/errors. NAS copyting takes precedence.
+if not exist %temp%\damo_net\logs\ (mkdir %temp%\damo_net\logs\)
+if not exist %temp%\damo_net\trusted-root-certificates (mkdir %temp%\damo_net\trusted-root-certificates)
+if not exist C:\damo_net\windows_enforcer\batch\install-removal (mkdir C:\damo_net\windows_enforcer\install-removal)
+if not exist C:\damo_net\scripts (mkdir C:\damo_net\scripts)
+if not exist C:\damo_net\windows_enforcer\batch\winget-installs.txt (echo. 2>C:\damo_net\windows_enforcer\batch\winget-installs.txt)
+if not exist C:\damo_net\windows_enforcer\batch\winget-uninstalls.txt (echo. 2>C:\damo_net\windows_enforcer\batch\winget-uninstalls.txt)
+if not exist C:\damo_net\windows_enforcer\batch\github-release-install.txt (echo. 2>C:\damo_net\windows_enforcer\batch\github-release-install.txt)
+if not exist C:\damo_net\windows_enforcer\batch\github-raw-installs.txt (echo. 2>C:\damo_net\windows_enforcer\batch\github-raw-installs.txt)
+if not exist C:\damo_net\windows_enforcer\batch\github-release-install.txt (echo. 2>C:\damo_net\windows_enforcer\batch\github-release-install.txt)
+if not exist C:\damo_net\windows_enforcer\batch\install-removal\installed.txt (echo. 2>C:\damo_net\windows_enforcer\batch\install-removal\installed.txt)
+if not exist C:\damo_net\windows_enforcer\batch\install-removal\install_desired.txt (echo. 2>C:\damo_net\windows_enforcer\batch\install-removal\install_desired.txt)
+
+:: Check dependencies
+winget -v
+if not errorlevel 0 (
+	echo "*** Dependency Check - Winget not installed. Install 'App Installer' from the Microsoft Store for winget install feature ***" >> %LOGFILE%
+	exit
+)
+
+:: Main entry point for running enforcement and syncing data for Damo.net admin settings. Never rename or delete this file.
+
+:: check if on DAMO.NET workgroup (router name must match)
+echo checking if on proper DAMO.NET workgroup >> %LOGFILE%
+:: copy and paste the below 2 lines for each network, updating the FQDN of the router hostname and IP and updating the integer value for neterror array
+ping -n 1 10.10.0.1 | find "TTL" && ping -n 1 -a 10.10.0.1 | find "RT-AC5300-C300" && ipconfig | find "DAMO.NET"
+set neterror[0]=%errorlevel%
+
+:: loop through all error levels assinged to array and find a 0. Update the third number to number of home networks being checked minus 1.
+for /L %%n in (0,1,0) do (
+	echo !neterror[%%n]! >> %LOGFILE%
+	if !neterror[%%n]!==0 (goto start)
+	)
+
+:: exit with message if no 0 error levels found
+echo ** DAMO.NET home network not detected, exiting ** >> %LOGFILE%
+exit
+
+:start
+echo ** DAMO.NET workgroup detected, continuing... ** >> %LOGFILE%
+:: copy itself if file does not exist in designated location
+if not exist C:\damo_net\windows_enforcer\first_run_enforcement_checks.bat (
+	echo first run enforcement script does not exist, copying and executing remaining enforcement checks >> %LOGFILE%
+	mkdir C:\damo_net\windows_enforcer
+	copy "%~f0" C:\damo_net\windows_enforcer\first_run_enforcement_checks.bat
+)
+
+:: peform file sync from designated NAS folder attempting to map the correct drive letter if it does not exist
+echo running robo copy from central NAS enforcement folder to ensure latest files are being used >> %LOGFILE%
+:: check if drive is mapped, if not, ping NAS, if fails, skip, otherwise, re-map
+if not exist Z:\ (
+	echo "*** mapped drive not found, pinging NAS ***" >> %LOGFILE%
+	:: use IP of NAS device
+	ping -n 1 10.10.0.1 | find "TTL"
+	if not errorlevel 1 (
+		echo re-mapping NAS, ping responded >> %LOGFILE%
+		goto resync
+	) else (
+		echo "*** cannot ping NAS, either down or off the network ***" >> %LOGFILE%
+		echo "*** will attempt remaining enforcement tasks, which will fail if at least one sync has not occured ***" >> %LOGFILE%
+	) 
+) else (
+	if exist Z:\damo_net\windows_enforcer\batch\first_run_enforcement_checks.bat (
+		goto resync
+	) else (
+		echo "*** Z drive mapping is incorrect. Un-map existing Z drive, then try again ***" >> %LOGFILE%
+	)
+)
+
+:resync
+echo ** syncing files with NAS enforcement folder ** >> %LOGFILE%
+:: sync scripts
+robocopy C:\test\ C:\damo_net\ /MIR >> %LOGFILE%
+:: sync trusted root certificates
+robocopy C:\test\trusted-root-certificates %temp%\damo_net\trusted-root-certificates /MIR  >> %LOGFILE%
+
+echo "*tamper protection*" >> %LOGFILE%
+:: lock down local C:\scripts directory and all child objects
+echo "**locking down C:\Scripts directory**" >> %LOGFILE%
+icacls C:\damo_net\ /reset /q /c /t >> %LOGFILE%
+icacls C:\damo_net\ /inheritance:d >> %LOGFILE%
+icacls C:\damo_net\ /setowner "Administrators" /q /c /t >> %LOGFILE%
+icacls C:\damo_net\ /remove:g "Users" /q /c /t >> %LOGFILE%
+icacls C:\damo_net\ /remove:g "Authenticated Users" /q /c /t >> %LOGFILE%
+icacls C:\damo_net\ /grant "Users":(R) /q /c /t >> %LOGFILE%
+:: remove delete permissions for this script from the Administrators group
+icacls C:\damo_net\windows_enforcer\batch\first_run_enforcement_checks.bat /inheritance:d >> %LOGFILE%
+icacls C:\damo_net\windows_enforcer\batch\first_run_enforcement_checks.bat /remove:g "Administrators" /q /c >> %LOGFILE%
+icacls C:\damo_net\windows_enforcer\batch\first_run_enforcement_checks.bat /grant "Administrators":(RX) /q /c >> %LOGFILE%
+
+:: locking down task scheduler - prevent deletion
+@REM reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Task Scheduler5.0" /v "Task Deletion" /t REG_DWORD /d 0 /f
+
+:: remove all certs before installing for hosts opted into force cert refresh list with flag set to ON
+if exist C:\damo_net\windows_enforcer\batch\featureFlags-first-run_enforcement_checks\certificate-refresh_FORCE_renameMe-ON.txt (
+	FOR /F "tokens=* USEBACKQ" %%F IN (`hostname`) DO (
+    SET hostname=%%F
+	  findstr /r /s /i /m /c:"\<!hostname!\>" "C:\damo_net\windows_enforcer\batch\featureFlags-first-run_enforcement_checks\certificate-refresh_FORCE_renameMe-ON.txt"
+	  if not errorlevel 1 (
+		  echo 'feature flag for force cert refresh active with local host opted in. removing trusted root certificates listed in force list before installing' >> %LOGFILE%
+		  for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\featureFlags-first-run_enforcement_checks\certificate-refresh_FORCE_LIST.txt) do certutil.exe -delstore root %%A && echo ** Removed Cert Name: %%A **  >> %LOGFILE%
+	  )  
+  )
+)
+
+:: remove certs within revokation list
+if exist C:\damo_net\windows_enforcer\batch\cert-removal\cert-revoked.txt (
+	echo "applying cert revocation for certs specified in cert-revoked.txt"  >> %LOGFILE%
+	for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\cert-removal\cert-revoked.txt) do (
+		certutil.exe -verifystore root %%A
+		if errorlevel 0 (
+			certutil.exe -delstore root %%A && echo ** Cert Name: %%A **  >> %LOGFILE%
+		)	
+	)
+)
+
+:: remove before installing certs if feature flag active - via present file name and its list of 'Issued To' cert names (usefull for replacing expired certs)
+if exist C:\damo_net\windows_enforcer\batch\featureFlags-first-run_enforcement_checks\certificate-refresh_renameMe-ON.txt (
+	echo "always active feature flag - removing trusted root certificates listed in file before installing"  >> %LOGFILE%
+	for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\featureFlags-first-run_enforcement_checks\certificate-refresh_renameMe-ON.txt) do certutil.exe -delstore root %%A && echo ** Cert Name: %%A **  >> %LOGFILE%
+)
+
+:: install trusted root certificates if not present on local host
+echo installing trusted root certificates  >> %LOGFILE%
+for /f %%f in ('dir /b %temp%\damo_net\trusted-root-certificates\') do certutil.exe -addstore root %temp%\damo_net\trusted-root-certificates\%%f && echo ** File: %%f **  >> %LOGFILE%
+
+:: Ensure enablement for auditing/logging logon and logoff events
+echo *ensure enablement of audit logging for logon and logoff events* >> %LOGFILE%
+auditpol /set /subcategory:"Logoff" /success:enable /failure:enable >> %LOGFILE%
+auditpol /set /subcategory:"Logon" /success:enable /failure:enable >> %LOGFILE%
+
+:: Ensure enablement of task scheduler history
+echo *ensure enablement of task scheduler history* >> %LOGFILE%
+wevtutil set-log Microsoft-Windows-TaskScheduler/Operational /enabled:true >> %LOGFILE%
+
+:: check if the scheduled tasks exists, recreating & first running them as neccessary
+echo checking scheduled tasks >> %LOGFILE%
+schtasks /query /TN "Damo.net\First Run Enforcement Checks" >NUL 2>&1
+if %errorlevel% NEQ 0 (
+	echo scheduled task does not exist, re-creating - first run enforcement checks >> %LOGFILE%
+        :: create then start after delay while continuing this script
+	schtasks /Create /XML "C:\damo_net\windows_enforcer\batch\First Run Enforcement Checks.xml" /TN "Damo.net\First Run Enforcement Checks" >> %LOGFILE%
+	echo first run enforcement scheduled task will start in 10 seconds to intialize hourly auto-runs >> %LOGFILE%
+	start "" /b cmd /c "timeout /nobreak 10 >nul & start schtasks /run /TN "Damo.net\First Run Enforcement Checks" >> %LOGFILE%
+)
+
+schtasks /query /TN "Damo.net\Network Adapters - All - Reset" >NUL 2>&1
+if %errorlevel% NEQ 0 (
+	echo scheduled task does not exist, re-creating - network adapter reset script >> %LOGFILE% 
+	schtasks /Create /XML "C:\damo_net\windows_enforcer\powershell\Network Adapters - All - Reset.xml" /TN "Damo.net\Network Adapters - All - Reset" >> %LOGFILE%
+	schtasks /run /TN "Damo.net\Network Adapters - All - Reset" >> %LOGFILE%
+)
+
+:: Windows OS Settings
+echo **Windows OS Settings Enforcement** >> %LOGFILE%
+
+:: File Explorer - Enable Viewable Extensions
+reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt | findstr "0x0"
+if %errorlevel% NEQ 0 (
+	reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
+	echo "File Explorer - Enable Viewable Extensions -- reg key/value not found. Registry updated" >> %LOGFILE%
+)
+
+:: Install Tools
+echo **Install Process** >> %LOGFILE%
+
+:: winget installs
+echo **winget installs** >> %LOGFILE%
+
+for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\winget-installs.txt) do (
+	winget list %%A >> %LOGFILE%
+	if not errorlevel 0 (
+	winget install %%A -h --accept-package-agreements --accept-source-agreements --no-upgrade >> %LOGFILE% && echo installed %%A >> %LOGFILE%
+	) else (
+		echo "winget package already installed - %%A" >> %LOGFILE%
+	)
+)
+
+:: non-winget installs & downloads
+echo **non-winget installs and downloads** >> %LOGFILE%
+
+if not exist C:\Tools (
+	mkdir C:\Tools
+	echo created C:\Tools directory >> %LOGFILE%
+)
+
+:: GitHub Raw Installs
+echo **github raw installs** >> %LOGFILE%
+
+@echo off
+setlocal enabledelayedexpansion
+
+for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\github-raw-installs.txt) do (
+  set fullpath=%%A
+  for %%a in ("!fullpath!/.") do set lastPart=%%~nxa
+
+  for /f "tokens=1 delims=." %%a in ("!lastPart!") do (
+   set dirname=%%a
+   if not exist C:\Tools\!dirname! (
+    mkdir C:\Tools\!dirname! >> %LOGFILE%
+    curl -o C:\Tools\!dirname!\!lastPart! !fullpath! >> %LOGFILE%
+    echo !dirname! installed >> %LOGFILE%
+   )
+  )  
+)
+
+:: GitHub Release Installes
+echo **github release installs** >> %LOGFILE%
+
+for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\github-release-install.txt) do (
+set ghrelease=%%A
+
+for /f "tokens=2 delims=/" %%a in ("!ghrelease!") do (
+  set dirname2=%%a  
+  if not exist C:\Tools\!dirname2! (
+    mkdir C:\Tools\!dirname2! >> %LOGFILE% 
+    cd C:\Tools\!dirname2!
+    for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/!ghrelease!/releases/latest ^| find "browser_download_url"') do (
+     curl -kOL %%B >> %LOGFILE%
+    )
+  )
+ )
+)
+
+:: Remove installs which are no longer listed - Excludes winget installs
+echo created list of what is installed >> %LOGFILE%
+dir /a:d /b C:\Tools\ > C:\damo_net\windows_enforcer\batch\install-removal\installed.txt
+
+echo created install_desired with list of what should be installed >> %LOGFILE%
+if exist C:\damo_net\windows_enforcer\batch\install-removal\install_desired.txt (
+del /Q C:\damo_net\windows_enforcer\batch\install-removal\install_desired.txt
+)
+for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\github-raw-installs.txt) do (
+  set fullpath=%%A
+  for %%a in ("!fullpath!/.") do set lastPart=%%~nxa
+  for /f "tokens=1 delims=." %%a in ("!lastPart!") do (
+   set dirname=%%a
+   echo !dirname! >> C:\damo_net\windows_enforcer\batch\install-removal\install_desired.txt
+  ) 
+)
+
+for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\github-release-install.txt) do (
+set ghrelease=%%A
+for /f "tokens=2 delims=/" %%a in ("!ghrelease!") do (
+  set dirname2=%%a
+  echo !dirname2! >> C:\damo_net\windows_enforcer\batch\install-removal\install_desired.txt
+ )
+)
+
+echo reconciled the list of installed against what should be installed outputting only those that should be removed >> %LOGFILE%
+if exist C:\damo_net\windows_enforcer\batch\install-removal\install_remove.txt (
+  del /Q C:\damo_net\windows_enforcer\batch\install-removal\install_remove.txt
+)
+
+for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\install-removal\installed.txt) do (
+  set installed=%%A
+  findstr "\<!installed!\>" "C:\damo_net\windows_enforcer\batch\install-removal\install_desired.txt"
+  if errorlevel 1 (
+    echo !installed! >> C:\damo_net\windows_enforcer\batch\install-removal\install_remove.txt
+  )
+)
+
+if exist C:\damo_net\windows_enforcer\batch\install-removal\install_remove.txt (
+  for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\install-removal\install_remove.txt) do (
+    set remove=%%A
+    rmdir /s/q C:\Tools\!remove!
+	echo removing !remove! >> %LOGFILE%
+  )
+)
+
+:: Remove winget installs listed within the winget-uninstalls input file
+echo **processing winget uninstalls** >> %LOGFILE%
+for /F "tokens=*" %%A in (C:\damo_net\windows_enforcer\batch\winget-uninstalls.txt) do (
+	set wgremove=%%A
+	findstr /r /s /i /m /c:"\<!wgremove!\>" "C:\damo_net\windows_enforcer\batch\winget-installs.txt"
+	if not errorlevel 1 (
+		echo "winget uninstall conflict for '!wgremove!' - present in install input as well, skipping" >> %LOGFILE%
+	) else (
+		: winget install !wgremove! --force --override "/uninstall /silent"
+		echo attempting winget uninstall - !wgremove! >> %LOGFILE%
+		winget uninstall !wgremove! -h --force --purge --disable-interactivity >> %LOGFILE%
+		if not errorlevel 0 (
+			echo "package already uninstalled or not found - !wgremove!" >> %LOGFILE%
+			) else (
+				echo uninstalled winget install - !wgremove! >> %LOGFILE%
+			) 
+	)
+)
+
+echo *End Installs* >> %LOGFILE%
+echo *End Script* >> %LOGFILE%
+
+:: Output log file location
+echo LOG LOCATION: %LOGFILE%
